62b97c37a49fb2b7ed0598de855857e95e2292e0
NightGhost <maximgradan@gmail.com>
2018-11-04 17:05:38 +0300

    Address storage debug added.

---
+++ AddressStorageDebug/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DriveAccessors;
+using System.IO;
+
+namespace AddressStorageDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Create("addresses.dat").Close();
+
+            AddressStorage addressStorage = new AddressStorage("addresses.dat");
+
+            addressStorage.Add(13);
+            addressStorage.Add(32);
+            addressStorage.Add(6);
+
+            Console.WriteLine($"Addres #2: {addressStorage[1]}");
+            Console.WriteLine();
+
+            foreach (long address in addressStorage)
+                Console.WriteLine($"Addres: {address}");
+        }
+    }
+}
